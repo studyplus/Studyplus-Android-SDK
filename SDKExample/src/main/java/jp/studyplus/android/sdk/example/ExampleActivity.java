1623d325a780d94128da51af2be5a010af42a7d0
haco <haco@studyplus.jp>
2014-02-14 12:00:34 +0900

    Remove unused Menu

--- SDKExample/src/main/java/jp/studyplus/android/sdk/example/ExampleActivity.java
+++ SDKExample/src/main/java/jp/studyplus/android/sdk/example/ExampleActivity.java
@@ -6,8 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -92,25 +90,5 @@ public class ExampleActivity extends ActionBarActivity {
 			);
 		}
 	}
-
-	@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.example, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
 }
