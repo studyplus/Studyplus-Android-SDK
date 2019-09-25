b1153718ac147e1a716ddcbacaa3996c7efede30
Koji Wakamiya <koji.wakamiya@studyplus.jp>
2019-09-25 15:15:32 +0900

    Add open Google Play Store logic

--- studyplus-android-sdk/src/main/java/jp/studyplus/android/sdk/internal/auth/AuthTransit.kt
+++ studyplus-android-sdk/src/main/java/jp/studyplus/android/sdk/internal/auth/AuthTransit.kt
@@ -1,8 +1,11 @@
 package jp.studyplus.android.sdk.internal.auth
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import androidx.core.net.toUri
 import jp.studyplus.android.sdk.R
 
 internal class AuthTransit
@@ -11,20 +14,37 @@ internal class AuthTransit
  * @param consumerSecret for API
  */
 constructor(
-        private val consumerKey: String,
-        private val consumerSecret: String
+    private val consumerKey: String,
+    private val consumerSecret: String
 ) {
     companion object {
         private const val EXTRA_CONSUMER_KEY = "consumer_key"
         private const val EXTRA_CONSUMER_SECRET = "consumer_secret"
+
+        private val URI_STORE = "market://details?id=jp.studyplus.android.app".toUri()
+        private val INTENT_STORE = Intent(Intent.ACTION_VIEW, URI_STORE)
     }
 
     fun start(activity: Activity, requestCode: Int) {
+        if (isInstalledStudyplus(activity).not()) {
+            openGooglePlayStore(activity)
+            return
+        }
+
         val intent = Intent()
         intent.action = Intent.ACTION_VIEW
         intent.putExtra(EXTRA_CONSUMER_KEY, consumerKey)
         intent.putExtra(EXTRA_CONSUMER_SECRET, consumerSecret)
         intent.data = Uri.parse(activity.getString(R.string.app_custom_scheme))
         activity.startActivityForResult(intent, requestCode)
+    }
+
+    private fun openGooglePlayStore(context: Context) {
+        context.startActivity(INTENT_STORE)
+    }
+
+    private fun isInstalledStudyplus(context: Context): Boolean {
+        val packages = context.packageManager.getInstalledApplications(PackageManager.GET_META_DATA)
+        return packages.any { it.packageName == "jp.studyplus.android.app" }
     }
 }
